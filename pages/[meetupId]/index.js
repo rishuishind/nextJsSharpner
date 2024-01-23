d503e8527368b853c04ed6f65d0965fb15f77044
rishabh <hespnod2348@gmai.com>
2024-01-23 16:08:17 +0530

    made the getstaticprops and paths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,29 @@
-import { useRouter } from 'next/router';
 import Dummy_Meetups from '../../components/data/Dummy_Meetups';
 
-const showMeetup = () => {
-    const router = useRouter();
-    const id = router.query.meetupId;
-    const data = Dummy_Meetups.find((list) => list.id === id)
+const showMeetup = (props) => {
+
     return <>
-        <img src={data.image} alt={data.title} />
-        <h3>{data.title}</h3>
-        <h4>{data.address}</h4>
+        <img src={props.data.image} alt={props.data.title} />
+        <h3>{props.data.title}</h3>
+        <h4>{props.data.address}</h4>
     </>
+}
+export function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [{ params: { meetupId: 'm1', } }, { params: { meetupId: 'm2' } }, { params: { meetupId: 'm3' } }]
+    }
+}
+export function getStaticProps(context) {
+    const id = context.params.meetupId;
+    let data = Dummy_Meetups.find((list) => list.id === id)
+    if (!data) {
+        data = Dummy_Meetups[1];
+    }
+    return {
+        props: {
+            data: data,
+        }
+    }
 }
 export default showMeetup
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import MeetupList from "../components/meetups/MeetupList";
 import Dummy_Meetups from '../components/data/Dummy_Meetups';
 
-const homePage = () => {
+const homePage = (props) => {
     return <>
-        <MeetupList meetups={Dummy_Meetups} />
+        <MeetupList meetups={props.meetup} />
     </>
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            meetup: Dummy_Meetups,
+        },
+        revalidate: 1
+    }
 }
 
 export default homePage;
